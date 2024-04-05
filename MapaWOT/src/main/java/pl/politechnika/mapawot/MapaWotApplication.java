24ecca9f67eda339821134266ac8e99eced4868a
poi-185 <253463@edu.p.lodz.pl>
2024-04-06 01:29:46 +0200

    Added first map using Leaflet JS with markers. Still working on JSON fetching.

---
+++ MapaWOT/src/main/java/pl/politechnika/mapawot/MapaWotApplication.java
@@ -0,0 +1,13 @@
+package pl.politechnika.mapawot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaWotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaWotApplication.class, args);
+	}
+
+}
---
+++ MapaWOT/src/test/java/pl/politechnika/mapawot/MapaWotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.politechnika.mapawot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaWotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
