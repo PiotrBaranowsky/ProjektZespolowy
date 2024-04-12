365ab78873e9372c368fbcd005a569ef29172828
poi-185 <253463@edu.p.lodz.pl>
2024-04-13 00:41:27 +0200

    CORS policy added. Map reads only first soldier JSON multiply times. Static images don't load.

--- MapaWOT/src/main/java/pl/politechnika/mapawot/MapaWotController.java
+++ MapaWOT/src/main/java/pl/politechnika/mapawot/MapaWotController.java
@@ -1,17 +1,15 @@
 package pl.politechnika.mapawot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MapaWotController {
-    @RequestMapping
-    public String helloWorld(){
-        return "Hello world";
-    }
-
-    @RequestMapping("/goodbye")
-    public String goodbye(){
-        return "Goodbye";
+    @RequestMapping("/map")
+    public String map(){
+        return "map";
     }
 }
